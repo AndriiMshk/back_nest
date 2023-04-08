cf09b55199532686a250972c5914558126870989
AndrMshk <andrmshk1@gmail.com>
2023-04-08 20:34:58 +0300

    add authorization guard

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger'
+
 export class CreateRoleDto {
+	@ApiProperty({ example: 'user', description: 'role_description' })
 	readonly value: string
+	@ApiProperty({ example: 'user', description: 'role_description' })
 	readonly description: string
 }
--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,16 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common'
 import { RolesService } from './roles.service'
 import { CreateRoleDto } from './dto/create-role.dto'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { Role } from './roles.model'
 
+@ApiTags('roles')
 @Controller('roles')
 export class RolesController {
 	constructor(private roleService: RolesService) {}
 
+	@ApiOperation({ summary: 'create user role' })
+	@ApiResponse({ status: 200, type: Role })
 	@Post()
-	create(@Body() dto: CreateRoleDto) {
-		return this.roleService.createRole(dto)
+	create(@Body() roleDto: CreateRoleDto) {
+		return this.roleService.createRole(roleDto)
 	}
 
+	@ApiOperation({ summary: 'get users roles' })
+	@ApiResponse({ status: 200, type: [Role] })
 	@Get('/:value')
 	getByValue(@Param('value') value: string) {
 		return this.roleService.getRoleByValue(value)
