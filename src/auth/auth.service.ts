574eeb9c49034f5bdb050023f3b94af402253bb6
AndrMshk <andrmshk1@gmail.com>
2023-04-08 19:30:32 +0300

    login ability

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common'
 import { CreateUserDto } from '../users/dto/create-user.dto'
 import { UsersService } from '../users/users.service'
 import { JwtService } from '@nestjs/jwt'
@@ -9,7 +9,10 @@ import { User } from '../users/users.model'
 export class AuthService {
 	constructor(private userService: UsersService, private jwtService: JwtService) {}
 
-	async login(userDto: CreateUserDto) {}
+	async login(userDto: CreateUserDto) {
+		const user = await this.validateUser(userDto)
+		return this.generateToken(user)
+	}
 
 	async register(userDto: CreateUserDto) {
 		const candidate = await this.userService.getUserByEmail(userDto.email)
@@ -21,10 +24,24 @@ export class AuthService {
 		return this.generateToken(user)
 	}
 
-	async generateToken(user: User) {
+	private async generateToken(user: User) {
 		const payload = { email: user.email, id: user.id, roles: user.roles }
 		return {
 			token: this.jwtService.sign(payload),
+		}
+	}
+
+	private async validateUser(userDto: CreateUserDto) {
+		const message = 'Invalid login or password'
+		const user = await this.userService.getUserByEmail(userDto.email)
+		if (!user) {
+			throw new UnauthorizedException({ message })
+		}
+		const passwordEquals = await bcrypt.compare(userDto.password, user.password)
+		if (user && passwordEquals) {
+			return user
+		} else {
+			throw new UnauthorizedException({ message })
 		}
 	}
 }
