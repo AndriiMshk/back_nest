606351b04833396a14da3885cc389c781e68e95b
AndrMshk <andrmshk1@gmail.com>
2023-04-08 18:21:17 +0300

    add role to user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,16 +2,23 @@ import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/sequelize'
 import { CreateUserDto } from './dto/create-user.dto'
 import { User } from './users.model'
+import { RolesService } from '../roles/roles.service'
 
 @Injectable()
 export class UsersService {
-	constructor(@InjectModel(User) private userRepository: typeof User) {}
+	constructor(
+		@InjectModel(User) private userRepository: typeof User,
+		private roleService: RolesService,
+	) {}
 
 	async createUser(dto: CreateUserDto) {
-		return await this.userRepository.create(dto)
+		const role = await this.roleService.getRoleByValue('user')
+		const user = await this.userRepository.create(dto)
+		await user.$set('role', [role.id])
+		return user
 	}
 
 	async getAllUsers() {
-		return await this.userRepository.findAll()
+		return await this.userRepository.findAll({ include: { all: true } })
 	}
 }
