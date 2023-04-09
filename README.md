b63a9c5aae192aef7ce1eb74bfc3247f1dca7b52
AndrMshk <andrmshk1@gmail.com>
2023-04-09 17:13:54 +0300

    add validation pipe

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
+
 <p align="center">
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
 </p>
 
 
-https://www.youtube.com/watch?v=dDeWWQWMM-Y&t=712s 1:06:20
+https://www.youtube.com/watch?v=dDeWWQWMM-Y&t=712s 1:20:35
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsEmail, IsString, Length } from 'class-validator'
 
 export class CreateUserDto {
 	@ApiProperty({ example: 'user@email.com', description: 'user email' })
+	@IsString({ message: 'must be string' })
+	@IsEmail({}, { message: 'invalid email' })
 	readonly email: string
 
 	@ApiProperty({ example: 'password', description: 'password' })
+	@IsString({ message: 'must be string' })
+	@Length(4, 16, { message: 'must be 4-16 characters' })
 	readonly password: string
 }
