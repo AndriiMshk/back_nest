182e76fc182c88a7683f363e064007f0a88dec03
AndrMshk <andrmshk1@gmail.com>
2023-04-09 17:32:36 +0300

    refactor swagger

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,8 +1,11 @@
 import { IsNumber, IsString } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class AddRoleDto {
+	@ApiProperty({ example: 5, description: 'user id' })
 	@IsNumber({}, { message: 'must be number' })
-	readonly userId: string
+	readonly userId: number
+	@ApiProperty({ example: 'admin', description: 'new user role' })
 	@IsString({ message: 'must be string' })
 	readonly value: string
 }
--- src/users/dto/ban-user.dto.ts
+++ src/users/dto/ban-user.dto.ts
@@ -1,4 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNumber, IsString } from 'class-validator'
+
 export class BanUserDto {
+	@ApiProperty({ example: 14, description: 'user id' })
+	@IsNumber({}, { message: 'must be number' })
 	readonly userId: number
+	@ApiProperty({ example: 'current reason', description: 'ban reason' })
+	@IsString({ message: 'must be string' })
 	readonly banReason: string
 }
